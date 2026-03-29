2771ecce6fe8a182b1f10722a82648dd153d4ebb
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-03-29 21:46:03 +0300

    splash screen layout'a eklendi

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
     <>
       <StatusBar style="auto" />
       <Stack screenOptions={{ headerShown: false }}>
-        {/* We will manage navigation via index.tsx auto-redirects based on Auth Status */}
         <Stack.Screen name="index" />
         <Stack.Screen name="(auth)" />
         <Stack.Screen name="(tabs)" />
